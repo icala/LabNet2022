4157e256c499354087f3de563b01e11f28992742
Gustavo Alaci <gustavoalaci@gmail.com>
2022-06-17 20:21:44 -0300

    consumo de api Universidades

---
+++ Lab.Demo.EF/Lab.Demo.EF.MVC/Controllers/UniversitiesController.cs
@@ -0,0 +1,49 @@
+﻿using Lab.Demo.EF.MVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Demo.EF.MVC.Views
+{
+    public class UniversitiesController : Controller
+    {
+        // GET: Universidades
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(string pais)
+        {
+            string Baseurl = "http://universities.hipolabs.com/";
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                var encodedPais = HttpUtility.UrlEncode(pais);
+                HttpResponseMessage Res = await client.GetAsync($"search?country={encodedPais}");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var response = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    var universidades = JsonConvert.DeserializeObject<List<UniversidadDTO>>(response);
+                    return View("Lista", universidades);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Lab.Demo.EF/Lab.Demo.EF.MVC/Models/UniversidadDTO.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab.Demo.EF.MVC.Models
+{
+    public class UniversidadDTO
+    {
+        public List<string> domains { get; set; }
+        public List<string> web_pages { get; set; }
+
+        [JsonProperty("state-province")]
+        public string StateProvince { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public string alpha_two_code { get; set; }
+    }
+}
